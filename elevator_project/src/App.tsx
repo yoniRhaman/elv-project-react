e0c1a353ef3309d4908ef557461611ee096ef13c
yonatan.r <y.rahman770770@gmail.com>
2024-08-15 15:34:57 +0300

    I have done many buildings

--- elevator_project/src/App.tsx
+++ elevator_project/src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Building from "./components/Building/Building";
-import Elevator from "./components/Elevator/Elevator"; // Add this line
 import "./reset.css";
 import "./index.css";
 
 function App() {
   return (
-    <div className="App row gap-20">
+    <div
+      className="App"
+      style={{ display: "flex", flexDirection: "row", gap: "160px" }}
+    >
       {/* Example Building with Floors and an Elevator */}
       <Building numFloors={10} height={100} buildingId={1}>
+        {/* You can now pass children here if needed */}
+      </Building>
+      <Building numFloors={10} height={100} buildingId={2}>
         {/* You can now pass children here if needed */}
       </Building>
     </div>
